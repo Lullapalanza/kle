7e90b7bf90b5f1bf01d596cd8c99815bf00c4be7
jyrgen <jyrgen.luus@gmail.com>
2025-05-08 17:29:17 +0200

    add

--- kle/designs/RA00.py
+++ kle/designs/RA00.py
@@ -72,21 +72,31 @@ print("=== PL END ===")
 
 def get_interdigit_cap(layer, W, G, L, N):
     # Interdigit Cap
+    extra_end = 15
     interdigit_cap = KleLayoutElement()
     for n in range(N):
+        if n == 0:
+            _extra0 = -extra_end
+            _extra1 = 0
+        elif n == N-1:
+            _extra0 = 0
+            _extra1 = extra_end
+        else:
+            _extra0 = 0
+            _extra1 = 0
         interdigit_cap.add_element(
             create_shape(layer, [
-                [0, 0],
-                [W, 0],
-                [W, L],
-                [0, L]
+                [_extra0, 0],
+                [W + _extra1, 0],
+                [W + _extra1, L],
+                [_extra0, L]
             ]).move(n * (W + G), (n%2)*G)
         )
     end_conn = create_shape(layer, [
-        [-0, 0],
-        [-0, -W],
-        [(W + G) * N - G, -W],
-        [(W + G) * N - G, 0]
+        [-extra_end, 0],
+        [-extra_end, -W],
+        [extra_end + (W + G) * N - G, -W],
+        [extra_end + (W + G) * N - G, 0]
     ])
     interdigit_cap.add_element(end_conn)
     interdigit_cap.add_element(end_conn.get_copy().move(0, L + G + W))
@@ -116,6 +126,67 @@ def get_meander_path(height, step, N):
     return path
 
 
+def get_one_stage_of_bowtie():
+    cutout_width, cutout_height = 465, 226 + 20
+    bowtie = KleCutOut(create_shape(layers["SC"], [
+        (100-1, 22), (100-1, -cutout_height), (-cutout_width, -cutout_height), (-cutout_width, 22)
+    ]))
+
+    intercap, cap = get_interdigit_cap(layers["SC"], 6, 2, 120, 27)
+    bowtie.add_element(intercap.move(-138-7 + 15, -107*2 - 4 - 20 - 2))
+    bowtie.add_element(intercap.get_copy().flip_vertically().move(0, -224))
+
+    N_step = 28
+    gap_step = 20
+    gap_heigth = 80
+
+    ind_f_path = [(0, 0), (-gap_step, 0)]
+
+    def temp(blah):
+        if blah == 0:
+            return -gap_heigth
+        if blah == 1:
+            return -gap_heigth
+        if blah == 2:
+            return gap_heigth
+        else:
+            return gap_heigth
+
+    for i in range(N_step):
+        ind_f_path.append(
+            (-gap_step * (1 + (i+1)//2), temp(i%4)),
+        )
+
+    ind_f_path.append((-gap_step * (1 + (i+2)//2), 0))
+    ind_f_path.append((-310, 0))
+    ind_f_path.append((-320, 0))
+
+    inductor, len = get_routed_trace(layers["SC"], ind_f_path, width_start=2.5, width_end=2.5, radii=8)
+    tot_inductance = len * LSHEET/2
+    bowtie.add_element(inductor.move(-138-7, -5 - 107))
+    connection_ref = create_ref(0, -112)
+
+    bowtie.add_element(connection_ref)
+    print("bowtiecap", cap*2e12, "pF, bowtie Ind", tot_inductance*1e9, "nH")
+
+    return bowtie, connection_ref
+
+def get_trace_end(path):
+    trace, _ = get_routed_trace(layers["SC"], path, width_start=2.5, width_end=2.5, radii=4)
+    trace_and_endpoint = KleLayoutElement()
+    trace_and_endpoint.add_elements([
+        create_ref(*path[-1]), trace
+    ])
+
+    return trace_and_endpoint
+
+N_and_path = {
+    0: [(0, 0), (-70, 0), (-70, -270 + 19.25 - 200)],
+    1: [(0, 0), (-100, 0), (-250, -400), (-340 + 65.241, -400), (-350 + 65.241, -400)],
+    2: [(0, 0), (-100, 0), (-350 + 65.241, 0)],
+    3: [(0, 0), (-100, 0), (-250, 400), (-340 + 65.241, 400), (-350 + 65.241, 400)],
+    4: [(0, 0), (-70, 0), (-70, 270 - 19.274 + 200)]
+}
 
 def get_cascaded_LC(f, Z0, N, distance_from_PL):
     cap_N = 5
@@ -132,25 +203,73 @@ def get_cascaded_LC(f, Z0, N, distance_from_PL):
     lcp.meander_height = cL
     lcp.meander_L = mL
     lcp.meander_N = 0
-    lcp.cutout_width = 600
-    lcp.cutout_height = 673
+    lcp.cutout_width = 610
+    lcp.cutout_height = 673 + 400
+
+    DC_connect_zero = (-73.579 + 45 + 59.26 - 35.922, 200 + 114.5 + 142.5 + 3.75 - 10)
     
+    dc_endpoints = []
 
     cutout, resonators = get_interdigit_LC(layers["SC"], lcp)
-    resonators.move(45, 0)
-
+    resonators.move(45, 200 + (383.51 - 95 - 20) / 2)
     resonator_0 = resonators.get_copy()
+
+    trace = get_trace_end(N_and_path[0]).move(*DC_connect_zero)
+    resonators.add_element(trace)
+    dc_endpoints.append(trace.subelements[0])
+
     
     for i in range(1, N):
-        if i%2 == 1:
-            resonators.add_element(resonator_0.get_copy().move(0, (5 * 3 + cL) * i))
-        else:
-            resonators.add_element(resonator_0.get_copy().move(0, (5 * 3 + cL) * i))
+        resonators.add_element(resonator_0.get_copy().move(0, (5 * 3 + cL) * i))
+        trace = get_trace_end(N_and_path[i]).move(*DC_connect_zero).move(0, (5 * 3 + cL) * i)
+        resonators.add_element(trace)
+        dc_endpoints.append(trace.subelements[0])
+
+        if i in [1, 2, 3]:
+            for n in range(3):
+                bt_elem, bt_ref = get_one_stage_of_bowtie()
+                bt_elem.flip_horizontally()
+                bt_points = bt_ref.get_absolute_points()[0]
+                dc_trace_points = trace.subelements[0].get_absolute_points()[0]
+                bt_elem.move(dc_trace_points[0] - bt_points[0] - 465 - n * (465 + 99), dc_trace_points[1] - bt_points[1])
+                
+                resonators.add_element(bt_elem)
+
+        if i in [4]:
+            extra_parts = KleLayoutElement()
+
+            extra_conn = KleCutOut(create_shape(layers["SC"], [(0, 0), (400, 0), (400, 454), (0, 454)]))
+            extra_parts.add_element(extra_conn.move(-290, 1073))
+            start_point = dc_endpoints[i].get_absolute_points()[0]
+            extra_path = [start_point, (start_point[0], start_point[1] + 320), (start_point[0] - 214.911, start_point[1] + 320)]
+            extra_trace, _ = get_routed_trace(layers["SC"], extra_path, width_start=2.5, width_end=2.5, radii=10)
+            extra_conn.add_element(extra_trace)
+
+            endpoint = (start_point[0] - 214.911, start_point[1] + 320)
+            for n in range(3):
+                bt_elem, bt_ref = get_one_stage_of_bowtie()
+                bt_elem.flip_horizontally()
+                bt_points = bt_ref.get_absolute_points()[0]
+                dc_trace_points = endpoint
+                bt_elem.move(dc_trace_points[0] - bt_points[0] - 465 - n * (465 + 99), dc_trace_points[1] - bt_points[1])
+                
+                extra_parts.add_element(bt_elem)
+            
+            resonators.add_element(extra_parts)
+        
+    # add for 0
+    resonators.add_element(extra_parts.get_copy().flip_vertically().move(0, 1073))
+        
 
     resonators.move(310 - distance_from_PL, 0)
 
+    # print(dc_endpoints)
+
     return cutout
 
+
+# cap_elem, _ = get_interdigit_cap(layers["SC"], 6, 2, 120, 27)
+# layout.add_element(cap_elem.move(-200, -200))
 
 # def get_cascaded_square_LC(f, Z0, N):
 #     # mL, cL = get_L_length(f, Z0, width=2, L_sheet=LSHEET, N=11, eps=EPS)
@@ -207,8 +326,11 @@ DISTs_FROM_PL = []
 NUMBER_OF_RES = []
 
 distance_from_PL = 20
-for i in range(2, 7):
-    layout.add_element(get_cascaded_LC(7e9, 5000, i, distance_from_PL).move(i * 1000, 0))
+# for i in range(2, 7):
+
+layout.add_element(
+    get_cascaded_LC(6e9, 4000, 5, distance_from_PL)#.rotate_left().move(3178, 2257)
+)
 
 
 layout.build_to_file(
